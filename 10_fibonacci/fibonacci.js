a5d62668009c11c6a6d21a0545c8bc026b8eb58d
Nick <nickross999@hotmail.com>
2024-11-21 12:26:34 +0900

    finished all assignments

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let memberInt = parseInt(member);
+    if (memberInt < 0){
+        return "OOPS";
+    }
+    let fibonacciArray = [0, 1];
+    for (let i = 2; i <= memberInt; i++){
+        fibonacciArray.push(fibonacciArray[i-1] + fibonacciArray[i-2]);
+    }
+    return fibonacciArray[memberInt];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    let indexOfOldestPerson = 0;
+    let ageOfOldestPerson = 0;
+    for (let i = 0; i < peopleArray.length; i++){
+        if (!("yearOfDeath" in peopleArray[i])){
+            if (2024 - peopleArray[i].yearOfBirth > ageOfOldestPerson){
+                indexOfOldestPerson = i;
+                ageOfOldestPerson = 2024 - peopleArray[i].yearOfBirth;
+                }    
+        }
 
+        else if (peopleArray[i].yearOfDeath - peopleArray[i].yearOfBirth > ageOfOldestPerson){
+            indexOfOldestPerson = i;
+            ageOfOldestPerson = peopleArray[i].yearOfDeath - peopleArray[i].yearOfBirth;
+        }
+    }
+    return peopleArray[indexOfOldestPerson];
 };
 
 // Do not edit below this line
